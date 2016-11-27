e588ad60cce3af69c5486bfddf7e555840a9dd9e
Ivan Gladkov <ivan.gladkov@kodex.lv>
2016-11-27 20:50:40 +0200

    refactor formatter

--- Formatter.js
+++ Formatter.js
@@ -1,62 +1,85 @@
 function DiffFormatter()
 {
     this.diff = null;
+    this.container = null;
+    this.table = null;
+    this.lineCounter = 0;
 }
 DiffFormatter.prototype.format = function(diff, container)
 {
     this.diff = diff;
-
-    var self = this;
-    var table = $('<table></table>');
-    container.html(table);
+    this.container = container;
+    this.table = $('<table></table>');
+    this.container.html(this.table);
 
     for (var i = 0; i < diff.files.length; i++) {
-        var file = diff.files[i];
-        var fileHtml = '<tr class="file-row"><td colspan="2"><b>'+file.nameA+'</b></td><td colspan="2"><b>'+file.nameB+'</b></td></tr>';
-        var lineNum = 0;
-
-        for (var ii = 0; ii < file.chunks.length; ii++) {
-            var chunk = file.chunks[ii];
-            fileHtml += '<tr><td colspan="4"><hr></td></tr>';
-
-            for (var iii = 0; iii < chunk.versionA.rows.length; iii++) {
-                lineNum++;
-
-                var rowA = chunk.versionA.rows[iii];
-                var rowB = chunk.versionB.rows[iii];
-
-                var textA = self.escapeHtml(rowA.text || ' ');
-                var textB = self.escapeHtml(rowB.text || ' ');
-
-                var classA = '';
-                var classB = '';
-
-                if (rowA.type === '+') {
-                    classA = 'added';
-                } else if (rowA.type === '-') {
-                    classA = 'removed';
-                } else {
-                    classA = '';
-                }
-
-                if (rowB.type === '+') {
-                    classB = 'added';
-                } else if (rowB.type === '-') {
-                    classB = 'removed';
-                } else {
-                    classB = '';
-                }
-
-                fileHtml += '<tr><td class="line-num">'+lineNum+'</td><td class="'+classA+'"><pre>'+textA+'</pre></td><td class="line-num">'+lineNum+'</td><td class="'+classB+'"><pre>'+textB+'</pre></td></tr>';
-            }
-        }
-
-        table.append(fileHtml);
+        window.setTimeout(function(i) {
+            this.formatFile(diff.files[i]);
+        }.bind(this, i), 0);
     }
 
-    return table;
+    return this.table;
 };
-DiffFormatter.prototype.escapeHtml = function(text) {
+
+DiffFormatter.prototype.formatFile = function(file)
+{
+    this.lineCounter = 0;
+    var html = '<tr class="file-row"><td colspan="2"><b>'+file.nameA+'</b></td><td colspan="2"><b>'+file.nameB+'</b></td></tr>';
+
+    for (var i = 0; i < file.chunks.length; i++) {
+        html += this.formatChunk(file.chunks[i]);
+    }
+
+    this.table.append(html);
+};
+
+DiffFormatter.prototype.formatChunk = function(chunk)
+{
+    var html = '<tr><td colspan="4"><hr></td></tr>';
+
+    for (var i = 0; i < chunk.versionA.rows.length; i++) {
+        html += this.formatRow(chunk.versionA.rows[i], chunk.versionB.rows[i]);
+    }
+
+    return html;
+};
+
+DiffFormatter.prototype.formatRow = function(rowA, rowB)
+{
+    this.lineCounter++;
+
+    var textA = this.escapeHtml(rowA.text || ' ');
+    var textB = this.escapeHtml(rowB.text || ' ');
+
+    var classA = '';
+    var classB = '';
+
+    if (rowA.type === '+') {
+        classA = 'added';
+    } else if (rowA.type === '-') {
+        classA = 'removed';
+    } else {
+        classA = '';
+    }
+
+    if (rowB.type === '+') {
+        classB = 'added';
+    } else if (rowB.type === '-') {
+        classB = 'removed';
+    } else {
+        classB = '';
+    }
+
+    return '<tr>' +
+        '<td class="line-num">'+this.lineCounter+'</td>' +
+        '<td class="'+classA+'"><pre>'+textA+'</pre></td>' +
+        '<td class="line-num">'+this.lineCounter+'</td>' +
+        '<td class="'+classB+'"><pre>'+textB+'</pre></td>' +
+    '</tr>';
+};
+
+DiffFormatter.prototype.escapeHtml = function(text)
+{
     return text
         .replace(/&/g, "&amp;")
         .replace(/</g, "&lt;")
